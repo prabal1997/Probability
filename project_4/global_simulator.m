470e600a7fe5ce98284bd563f710864c63ac6f81
Prabal Gupta <prabal.gupta.97@gmail.com>
2017-11-03 10:34:44 -0400

    project_3 progress added

---
+++ project_4/global_simulator.m
@@ -0,0 +1,7 @@
+function [ output_args ] = global_simulator( input_args )
+%GLOBAL_SIMULATOR Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ project_4/request_simulator.m
@@ -0,0 +1,7 @@
+function [ output_args ] = request_simulator( input_args )
+%GLOBAL_SIMULATOR Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
